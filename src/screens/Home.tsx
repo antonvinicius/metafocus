b489affed80e9921d874b385fbec3da65ee08ec1
Antônio Vinícius <antoniovrdacosta@gmail.com>
2022-09-15 20:51:17 -0300

    feat: Add bottom tabs

---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading } from "native-base";
+import React from "react";
+
+export function Home() {
+  return (
+    <Box flex="1" alignItems="center" justifyContent="center">
+      <Heading>Home</Heading>
+    </Box>
+  );
+}
---
+++ src/screens/Profile.tsx
@@ -0,0 +1,10 @@
+import { Box, Heading } from "native-base";
+import React from "react";
+
+export function Profile() {
+  return (
+    <Box flex="1" alignItems="center" justifyContent="center">
+      <Heading>Perfil</Heading>
+    </Box>
+  );
+}
