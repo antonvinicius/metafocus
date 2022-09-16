366478b3845636ec905e501c18598199207adf07
Gabriel Dickman <gabrieldickman33@gmail.com>
2022-09-15 22:08:37 -0400

    Home Screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,12 @@
-import { Box, Heading } from "native-base";
+import { Box, Heading, Button, Icon } from "native-base";
+import Ionicons from 'react-native-vector-icons/Ionicons';
 import React from "react";
 
 export function Home() {
   return (
-    <Box flex="1" alignItems="center" justifyContent="center">
-      <Heading>Home</Heading>
+    <Box flex="1" alignItems="center" justifyContent="center" >
+      <Heading color="#A3A3A3">Você ainda não possui metas...</Heading>
+      <Button  endIcon={<Icon as={Ionicons} name="add" size="xl"/>} bgColor="#38B387" marginTop="1">Criar nova meta</Button>
     </Box>
   );
 }
