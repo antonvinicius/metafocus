a18ac76725e45acac89c3a0763e7e0491b168ac0
Antônio Vinícius <antoniovrdacosta@gmail.com>
2022-12-03 14:25:22 -0300

    feat: Add logout on home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,14 @@
-import { Box, Text } from "native-base";
+import { Box, Button, Text } from "native-base";
 import React from "react";
+import { useAuth } from "../hooks/useAuth";
 
 export function Home() {
+  const { setAuthenticated } = useAuth();
+
   return (
     <Box flex="1">
       <Text>Metas</Text>
+      <Button onPress={() => setAuthenticated(false)}>Sair</Button>
     </Box>
   );
 }
