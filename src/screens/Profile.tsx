20ebb62f9b0fa0a9f29d25a4a9747155dacf5691
Antônio Vinícius <antoniovrdacosta@gmail.com>
2022-12-04 01:55:06 -0300

    feat: Update profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,18 @@
-import { Box, Button, Divider, Heading, HStack, Icon, Text } from "native-base";
+import {
+  Box,
+  Button,
+  Divider,
+  Heading,
+  HStack,
+  Icon,
+  Image,
+  Text,
+  VStack,
+} from "native-base";
 import React from "react";
 import Feather from "react-native-vector-icons/Feather";
 import { useAuth } from "../hooks/useAuth";
+import { findByKey } from "../utils/AvatarsUtil";
 import { Results } from "./Results";
 
 export function Profile() {
@@ -15,7 +26,8 @@ export function Profile() {
       >
         Sair
       </Button>
-      <Box
+      <VStack
+        space={3}
         alignSelf="center"
         justifyContent="center"
         alignItems="center"
@@ -23,20 +35,17 @@ export function Profile() {
         h="100px"
         rounded="full"
       >
-        <Text fontSize="xl" color="white">
-          Vinicius
-        </Text>
-      </Box>
-      <Heading mt="3">Perfil</Heading>
-      <Divider my="2" />
-      <HStack justifyContent="space-between">
-        <HStack space="3" alignItems="center">
-          <Feather name="user" size={24} />
-          <Text fontSize="lg">Vini</Text>
-        </HStack>
-      </HStack>
-      <Divider my="2" />
-      <Results />
+        <Image
+          borderRadius="full"
+          backgroundColor="primary.400"
+          source={findByKey("man1").source}
+          w="90px"
+          h="90px"
+          alt="avatar"
+        />
+        <Heading>Vinícius</Heading>
+      </VStack>
+      {/* <Results /> */}
     </Box>
   );
 }
