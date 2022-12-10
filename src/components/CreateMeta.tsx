9f489befd21fbf39ce96109804d1e0544172128c
Antônio Vinícius <antoniovrdacosta@gmail.com>
2022-12-10 20:10:01 -0300

    wip: Add steps

--- src/components/CreateMeta.tsx
+++ src/components/CreateMeta.tsx
@@ -2,11 +2,13 @@ import {
   Box,
   Button,
   Divider,
+  FlatList,
   FormControl,
   HStack,
   Icon,
   IconButton,
   Input,
+  ScrollView,
   Stack,
   Text,
   TextArea,
@@ -24,11 +26,13 @@ import SelectBox from "react-native-multi-selectbox-typescript";
 import { selectExampleData } from "../data/chartDataExample";
 import { xorBy } from "lodash";
 import { theme } from "../global/theme";
+import { Step } from "./Step";
 
 export function CreateMeta() {
   const [goalDate, setGoalDate] = useState<Date | null>(null);
   const [selectedTeams, setSelectedTeams] = useState<any>([]);
   const { setModalVisible, modalVisible } = useModal();
+  const [steps, setSteps] = useState<any[]>([]);
 
   function handleCloseModal() {
     setModalVisible(false);
@@ -56,90 +60,127 @@ export function CreateMeta() {
     return (item: any) => setSelectedTeams(xorBy(selectedTeams, [item], "id"));
   }
 
+  function addNewStep() {
+    setSteps((old) => [...old, 1]);
+  }
+
+  function removeStep() {
+    const stepsClone = [...steps];
+    const newSteps = stepsClone.pop();
+    // TODO: Resolve error when try to remove empty array
+    if (newSteps != undefined) {
+      setSteps(newSteps);
+    }
+  }
+
   return (
-    <Box>
-      <Modal isVisible={modalVisible}>
-        <Box flex="1">
-          <VStack space="10px" px="4" flex="1" bg="white" borderRadius="lg">
-            <Button onPress={handleCloseModal}>Fechar</Button>
-            {/* Nome */}
-            <FormControl isRequired>
-              <FormControl.Label>Nome da meta</FormControl.Label>
-              <Input type="text" placeholder="Insira sua meta aqui 😅" />
-              <FormControl.ErrorMessage
-                leftIcon={<WarningOutlineIcon size="xs" />}
-              >
-                Pelo menos 3 caracteres são necessários.
-              </FormControl.ErrorMessage>
-            </FormControl>
+    <Modal isVisible={modalVisible} propagateSwipe>
+      <ScrollView>
+        <VStack space="10px" px="4" flex="1" bg="white" borderRadius="lg">
+          <Button onPress={handleCloseModal}>Fechar</Button>
+          {/* Nome */}
+          <FormControl isRequired>
+            <FormControl.Label>Nome da meta</FormControl.Label>
+            <Input type="text" placeholder="Insira sua meta aqui 😅" />
+            <FormControl.ErrorMessage
+              leftIcon={<WarningOutlineIcon size="xs" />}
+            >
+              Pelo menos 3 caracteres são necessários.
+            </FormControl.ErrorMessage>
+          </FormControl>
 
-            {/* Descrição */}
-            <FormControl isRequired>
-              <Box>
-                <FormControl.Label>Descrição da meta</FormControl.Label>
-                <TextArea
-                  numberOfLines={4}
-                  placeholder="Insira o que você deseja alcançar com esta meta ☕"
-                  autoCompleteType={undefined}
-                />
-              </Box>
-            </FormControl>
-
-            {/* Data final */}
-            <HStack alignItems="center">
-              <Button
-                variant="link"
-                onPress={showDatePicker}
-                leftIcon={
-                  <Icon
-                    as={Feather}
-                    name="calendar"
-                    size="25px"
-                    color="primary.600"
-                  />
-                }
-              >
-                {goalDate &&
-                  `Previsão de término em ${new Intl.DateTimeFormat(
-                    "pt-BR"
-                  ).format(goalDate as Date)}`}
-                {!goalDate && "Data final (opcional)"}
-              </Button>
-              {goalDate && (
-                <IconButton
-                  onPress={() => setGoalDate(null)}
-                  icon={
-                    <Icon as={Feather} name="x" size="24px" color="red.300" />
-                  }
-                />
-              )}
-            </HStack>
-
-            {/* Categorias */}
-            <FormControl isRequired>
-              <FormControl.Label>Selecione as Categorias</FormControl.Label>
-              <SelectBox
-                label="Categorias definirão quais áreas você irá subir de nível ao completar a meta"
-                inputPlaceholder="Selecione a categoria"
-                selectedItemStyle={{
-                  backgroundColor: theme.colors.primary,
-                }}
-                searchIconColor={theme.colors.primary}
-                toggleIconColor={theme.colors.primary}
-                arrowIconColor={theme.colors.primary}
-                hideInputFilter
-                options={selectExampleData}
-                selectedValues={selectedTeams}
-                onMultiSelect={onMultiChange()}
-                onTapClose={onMultiChange()}
-                isMulti
+          {/* Descrição */}
+          <FormControl isRequired>
+            <Box>
+              <FormControl.Label>Descrição da meta</FormControl.Label>
+              <TextArea
+                numberOfLines={4}
+                placeholder="Insira o que você deseja alcançar com esta meta ☕"
+                autoCompleteType={undefined}
               />
-            </FormControl>
+            </Box>
+          </FormControl>
 
-            {/* Etapas */}
-          </VStack>
-        </Box>
-      </Modal>
-    </Box>
+          {/* Data final */}
+          <HStack alignItems="center">
+            <Button
+              variant="link"
+              onPress={showDatePicker}
+              leftIcon={
+                <Icon
+                  as={Feather}
+                  name="calendar"
+                  size="25px"
+                  color="primary.600"
+                />
+              }
+            >
+              {goalDate &&
+                `Previsão de término em ${new Intl.DateTimeFormat(
+                  "pt-BR"
+                ).format(goalDate as Date)}`}
+              {!goalDate && "Data final (opcional)"}
+            </Button>
+            {goalDate && (
+              <IconButton
+                onPress={() => setGoalDate(null)}
+                icon={
+                  <Icon as={Feather} name="x" size="24px" color="red.300" />
+                }
+              />
+            )}
+          </HStack>
+
+          {/* Categorias */}
+          <FormControl isRequired>
+            <FormControl.Label>Selecione as Categorias</FormControl.Label>
+            <SelectBox
+              label="Categorias definirão quais áreas você irá subir de nível ao completar a meta"
+              inputPlaceholder="Selecione a categoria"
+              selectedItemStyle={{
+                backgroundColor: theme.colors.primary,
+              }}
+              searchIconColor={theme.colors.primary}
+              toggleIconColor={theme.colors.primary}
+              arrowIconColor={theme.colors.primary}
+              hideInputFilter
+              options={selectExampleData}
+              selectedValues={selectedTeams}
+              onMultiSelect={onMultiChange()}
+              onTapClose={onMultiChange()}
+              isMulti
+            />
+          </FormControl>
+
+          {/* Etapas */}
+          <FormControl>
+            <HStack justifyContent="space-between" alignItems="center">
+              <Box>
+                <FormControl.Label>Etapas (opcional)</FormControl.Label>
+                <FormControl.HelperText>
+                  Defina etapas para concluir sua meta
+                </FormControl.HelperText>
+              </Box>
+              <HStack space="2">
+                <Feather
+                  onPress={addNewStep}
+                  name="plus-circle"
+                  color={theme.colors.primary}
+                  size={24}
+                />
+                <Feather
+                  onPress={removeStep}
+                  name="minus-circle"
+                  color={theme.colors.primary}
+                  size={24}
+                />
+              </HStack>
+            </HStack>
+            {/* Item das etapas */}
+            <FlatList data={steps} renderItem={() => <Step />} />
+          </FormControl>
+        </VStack>
+      </ScrollView>
+    </Modal>
   );
 }
---
+++ src/components/Step.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import {
+  Box,
+  Button,
+  FormControl,
+  HStack,
+  Icon,
+  IconButton,
+  Input,
+  TextArea,
+  WarningOutlineIcon,
+} from "native-base";
+import Feather from "react-native-vector-icons/Feather";
+import { DateTimePickerAndroid } from "@react-native-community/datetimepicker";
+
+export function Step() {
+  const [goalDate, setGoalDate] = useState<Date | null>(null);
+
+  function onChange(event: any, selectedDate: any) {
+    const currentDate = selectedDate;
+    setGoalDate(currentDate);
+  }
+
+  function showMode(currentMode: any) {
+    DateTimePickerAndroid.open({
+      value: goalDate ?? new Date(),
+      onChange,
+      mode: currentMode,
+      is24Hour: true,
+    });
+  }
+
+  function showDatePicker() {
+    showMode("date");
+  }
+
+  return (
+    <>
+      {/* Nome */}
+      <FormControl isRequired>
+        <FormControl.Label>Nome da meta</FormControl.Label>
+        <Input type="text" placeholder="Insira sua meta aqui 😅" />
+        <FormControl.ErrorMessage leftIcon={<WarningOutlineIcon size="xs" />}>
+          Pelo menos 3 caracteres são necessários.
+        </FormControl.ErrorMessage>
+      </FormControl>
+
+      {/* Descrição */}
+      <FormControl isRequired>
+        <Box>
+          <FormControl.Label>Descrição da meta</FormControl.Label>
+          <TextArea
+            numberOfLines={4}
+            placeholder="Insira o que você deseja alcançar com esta meta ☕"
+            autoCompleteType={undefined}
+          />
+        </Box>
+      </FormControl>
+
+      {/* Data final */}
+      <HStack alignItems="center">
+        <Button
+          variant="link"
+          onPress={showDatePicker}
+          leftIcon={
+            <Icon
+              as={Feather}
+              name="calendar"
+              size="25px"
+              color="primary.600"
+            />
+          }
+        >
+          {goalDate &&
+            `Previsão de término em ${new Intl.DateTimeFormat("pt-BR").format(
+              goalDate as Date
+            )}`}
+          {!goalDate && "Data final (opcional)"}
+        </Button>
+        {goalDate && (
+          <IconButton
+            onPress={() => setGoalDate(null)}
+            icon={<Icon as={Feather} name="x" size="24px" color="red.300" />}
+          />
+        )}
+      </HStack>
+    </>
+  );
+}
