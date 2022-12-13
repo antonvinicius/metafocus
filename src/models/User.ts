5f2bcbe4f87d0d7c80c21a0eb8eeac023c5a8aa7
Antônio Vinícius <antoniovrdacosta@gmail.com>
2022-12-12 21:47:31 -0300

    feat: Add metas attribute to user

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,13 +1,16 @@
 import { defaultUserAttributes } from "../data/userAttributesRepository";
 import { Entity } from "./Entity";
+import { Meta } from "./Meta";
 import { UserAttribute } from "./UserAttribute";
 
 class User extends Entity {
   public attributes: UserAttribute[];
+  public metas: Meta[];
 
   constructor(public nickname: string, public avatar: string) {
     super();
     this.attributes = defaultUserAttributes;
+    this.metas = [];
   }
 }
 
