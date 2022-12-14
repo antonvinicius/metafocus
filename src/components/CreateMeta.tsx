111c40d4c83fe34e699eece350b7413c5fa3de9e
Antônio Vinícius <antoniovrdacosta@gmail.com>
2022-12-13 21:49:11 -0300

    feat: Resolve validation date not in past or same day

--- src/components/CreateMeta.tsx
+++ src/components/CreateMeta.tsx
@@ -44,10 +44,21 @@ export function CreateMeta() {
     setModalVisible(false);
   }
 
-  function onChange(event: any, selectedDate: any) {
+  function onChange(_: any, selectedDate: any) {
     const currentDate = selectedDate;
-    setGoalDate(currentDate);
-    // TODO: Set goal date not valid in the past
+    const dateToSave = new Date(currentDate);
+    dateToSave.setHours(0);
+    dateToSave.setMinutes(0);
+    dateToSave.setSeconds(0);
+    dateToSave.setMilliseconds(0);
+
+    const nowDate = new Date();
+    nowDate.setHours(0);
+    nowDate.setMinutes(0);
+    nowDate.setSeconds(0);
+    nowDate.setMilliseconds(0);
+
+    if (dateToSave > nowDate) setGoalDate(dateToSave);
   }
 
   function showMode(currentMode: any) {
